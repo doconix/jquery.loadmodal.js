d004f989f00e52c0f070be1d0272b99183c7993e
Conan C. Albrecht <ca@byu.edu>
2016-06-23 15:09:44 -0600

    message

--- jquery.loadmodal.js
+++ jquery.loadmodal.js
@@ -1,16 +1,16 @@
 /*
     Author: Conan C. Albrecht <ca@byu.edu>
     License: MIT
-    Version: 1.1.15 (Feb 2015)
+    Version: 1.1.16 (June 2016)
 
     Reminder on how to publish to GitHub:
         Change the version number in all the files.
         git commit -am 'message'
         git push origin master
-        git tag 1.1.14
+        git tag 1.1.16
         git push origin --tags
 
-    Dependencies: 
+    Dependencies:
       - JQuery 1.5+
       - Bootstrap (tested against v3)
 
@@ -18,7 +18,7 @@
     You can do this with standard Bootstrap, but this plugin makes it easier.  It
     does the <div> structure for you, loads the content, and opens the dialog.
 
-    Simple example: 
+    Simple example:
 
         $.loadmodal('/your/server/url/');
 
@@ -34,7 +34,7 @@
             "OK": function() {
               // do something here
               // a false return here cancels the automatic closing of the dialog
-            },  
+            },
             "Cancel": false,   // no-op - just having the option makes the dialog close
           },
           modal: {
@@ -56,73 +56,73 @@
         });
 
     Closing a dialog: (this is standard bootstrap)
-        
-        $('#my-modal-id').modal('hide'); 
+
+        $('#my-modal-id').modal('hide');
 
 
 */
 (function($) {
-  
+
 	$.loadmodal = function(options) {
 
     // allow a simple url to be sent as the single option
     if ($.type(options) == 'string') {
-      options = { 
+      options = {
         url: options,
       };//options
     }//if
-    
+
     // set the default options
     options = $.extend(true, {
       url: null,                               // a convenience place to specify the url - this is moved into ajax.url
-      
+
       id: 'jquery-loadmodal-js',               // the id of the modal
-      
+
       idBody: 'jquery-loadmodal-js-body',      // the id of the modal-body (the dialog content)
-      
+
       appendToSelector: 'body',                // the element to append the dialog <div> code to.  Normally, this should be left as the 'body' element.
-      
+
       title: window.document.title || 'Dialog',// the title of the dialog
-      
+
       width: '400px',                          // 20%, 400px, or other css width
-      
+
       dlgClass: 'fade',                        // CSS class(es) to add to the <div class="modal"> main dialog element.  This default makes the dialog fade in.
-      
+
       size: 'modal-lg',                        // CSS class(es) to specify the dialog size ('modal-lg', 'modal-sm', '').  The default creates a large dialog.  See the Bootstrap docs for more info.
-      
+
       closeButton: true,                       // whether to have an 'X' button at top right to close the dialog
-      
+
       buttons: {                               // set titles->functions to add buttons to the bottom of the dialog
       },//buttons                              // return false from the function to prevent the automatic closing of the dialog
-             
+
       modal: {                                 // options sent into $().modal (see Bootstrap docs for .modal and its options)
       },//modal
-      
+
       ajax: {                                  // options sent into $.ajax (see JQuery docs for .ajax and its options)
         url: null,                             // required (for convenience, you can specify url above instead)
       },//ajax
-      
-      beforeShow: null,                        // This method is called at the beginning of the default success method.  If this method 
-                                               // returns false (an explicit false), the default success method is canceled and not run.  You can also define 
+
+      beforeShow: null,                        // This method is called at the beginning of the default success method.  If this method
+                                               // returns false (an explicit false), the default success method is canceled and not run.  You can also define
                                                // normal ajax success methods in the ajax options above, but these are called *after* the
                                                // modal is shown.  This method allows you to handle the response before the modal is called.
-      
+
       onShow: null,                            // if set, this function will be called with a reference to the dialog once it has been
                                                // successfully shown.  You can also listen for modal events, but be aware that show.bs.modal
                                                // triggers *before* the content is added to .modal-body.
-      
-    }, options);  
-    
-      
+
+    }, options);
+
+
     // ensure we have a url
     options.ajax.url = options.ajax.url || options.url;
     if (!options.ajax.url) {
       throw new Error('$().loadmodal requires a url.');
     }//if
-    
+
     // close any dialog with this id first
     $('#' + options.id).modal('hide');
-    
+
     // create our own success responder for the ajax
     options.ajax.success = $.isArray(options.ajax.success) ? options.ajax.success : options.ajax.success ? [ options.ajax.success ] : [];
     options.ajax.success.unshift(function(data, status, xhr) { // unshift puts this as the first success method
@@ -130,11 +130,11 @@
       if (options.beforeShow && options.beforeShow(data, status, xhr) === false) {
         return;
       }//if
-      
+
       // create the modal html
       var div = $([
                       '<div id="' + options.id + '" class="modal ' + options.dlgClass + ' jquery-loadmodal-js">',
-                      '  <div class="modal-dialog ' + options.size + '">',
+                      '  <div class="modal-dialog ' + options.size + '" role="document">',  // role is for accessibility
                       '      <div class="modal-content">',
                       '        <div class="modal-header">',
 options.closeButton ? '          <button class="close" data-dismiss="modal" type="button">x</button>' : '',
@@ -146,13 +146,13 @@ options.closeButton ? '          <button class="close" data-dismiss="modal" type
                       '    </div>',
                       '  </div>',
       ].join('\n'));
-      
+
       // add the new modal div to the body and show it!
       $(options.appendToSelector).append(div);
       div.find('.modal-body').html(data);
       div.modal(options.modal);
       div.find('.modal-dialog').css('width', options.width);
-      
+
       // add buttons to the dialog, if any
       if (!$.isEmptyObject(options.buttons)) {
         div.find('.modal-body').append('<div class="button-panel"></div>');
@@ -178,33 +178,17 @@ options.closeButton ? '          <button class="close" data-dismiss="modal" type
         div.removeData();
         div.remove();
       });
-      
+
       // call the onShow function if there is one
       if (options.onShow) {
         options.onShow(div);
       }//if
-      
+
     });//unshift (add success method)
-    
+
     // load the content from the server
     $.ajax(options.ajax);
-    
+
   };//loadmodal top-level function
-  
+
 })(jQuery);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
